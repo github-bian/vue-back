114330d09b319dc75c508d17bb6bab791aa55a67
Genius <30947370+github-bian@users.noreply.github.com>
2022-05-30 09:14:10 +0800

    fix cross

--- server.js
+++ server.js
@@ -1,15 +1,13 @@
 let http = require("http")
 const users = [
-    { id: 1, name: '边龙' },
-    { id: 2, name: '张三' },
-    { id: 3, name: '李四' },
+    { id: 1, name: 'bian' },
+    { id: 2, name: 'tom' },
+    { id: 3, name: 'jack' },
 ]
 let server = http.createServer(function(req, res) {
     console.log("req", req.url)
-        // 为了防止中文乱码问题，需要设置响应头，
-    res.setHeader('Content-Type', 'text/html; charset=utf-8')
+    // 解决跨域，
     res.setHeader('Access-Control-Allow-Origin', '*')
-
     if (req.url === '/api/users') {
         res.end(JSON.stringify(users))
     } else {
